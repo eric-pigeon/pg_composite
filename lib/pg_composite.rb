9664daa987f99b48af6cfc9cb7f5d1250504379a
Eric Pigeon <eric.r.pigeon@gmail.com>
2020-03-07 16:27:22 -0500

    new gem

---
+++ lib/pg_composite.rb
@@ -0,0 +1,6 @@
+require "pg_composite/version"
+
+module PgComposite
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pg_composite/version.rb
@@ -0,0 +1,3 @@
+module PgComposite
+  VERSION = "0.1.0"
+end
